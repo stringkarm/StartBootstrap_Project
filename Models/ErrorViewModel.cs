baef26d5fc0351ad6f1c81b7db577934def2a2e8
ACT-STUDENT <ACT-STUDENT@LAB2-PC35>
2026-07-07 16:34:44 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StartBootstrap_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
