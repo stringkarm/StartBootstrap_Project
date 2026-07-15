164e39d9a30651b5fd9ade13719dabe34896bada
stringkarm <ccs.karmelieanncanasa@gmail.com>
2026-07-15 21:39:42 +0800

    updated the landing pages

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// 1. Add MVC Services
 builder.Services.AddControllersWithViews();
+
+// 2. REGISTER THE COOKIE HANDLER (This fixes your error!)
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        // If a user tries to access a restricted page, send them here
+        options.LoginPath = "/Account/Login";
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+    });
 
 var app = builder.Build();
 
@@ -9,7 +20,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -18,10 +28,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
+// 3. MIDDLEWARE ORDER IS CRITICAL
+app.UseAuthentication(); // Reads the cookie and logs the user in on every page load
+app.UseAuthorization();  // Checks if they are allowed to access specific pages
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
